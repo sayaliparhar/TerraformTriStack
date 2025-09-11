1d85318f67a64f7ad7296020f68bf3710cc879c9
Sayali Parhar <sayaliparhar26@gmail.com>
2025-09-11 23:25:50 +0530

    updated web1.tf

--- Modules/EC2/web1.tf
+++ Modules/EC2/web1.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "bastion-host" {
     ami = var.ami_id
     subnet_id = var.pub1_subnet_id
     associate_public_ip_address = true
-    key_name = "Project_key"
+    key_name = aws_key_pair.key.id
     vpc_security_group_ids = [var.public_security_group_ids]
     root_block_device {
       volume_size = 8
